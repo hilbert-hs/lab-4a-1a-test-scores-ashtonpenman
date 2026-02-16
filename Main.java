f624be3e079b54d46c6989fc3dc75c8bcbdb1818
ashtonpenman <88688089+ashtonpenman@users.noreply.github.com>
2026-02-16 17:07:27 +0000

    done with 1

--- Main.java
+++ Main.java
@@ -2,7 +2,20 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    
+    String[] answerArr = new String[20];
+    Scanner scan = new Scanner(System.in);
+
+    for(int i = 0; i < 20; i++){
+      System.out.println((i+1) + ".");
+      answerArr[i] = scan.nextLine().toUpperCase();
+    }
+
+    UnitTest test = new UnitTest(answerArr);
+    System.out.println(test.toString());
+    System.out.println("Number of Correct Answers: " + test.totalCorrect());
+    System.out.println("Number of Incorrect Answers: " + test.totalMistakes());
+    System.out.println("Did the Student Pass? " + test.isPassing());
+  
   }
 
 }
--- UnitTest.java
+++ UnitTest.java
@@ -1,13 +1,13 @@
 public class UnitTest {
 
   // instance variables
-  private final String[] answerKey = {"B", "D", "A", "E", "A", "B", "A", "E", "B", "B", "E", "E", "D", "D", "A", "C", "C", "A", "E"};
+  private final String[] answerKey = {"B", "D", "C", "A", "E", "A", "B", "A", "E", "B", "B", "E", "E", "D", "D", "A", "C", "C", "A", "E"};
   private String[] studentAns;
   
   // constructor
   public UnitTest(String[] arr){
     studentAns = new String[arr.length];
-    for(int i = 0 ; i <= arr.length ; i++){
+    for(int i = 0; i < arr.length ; i++){
       studentAns[i] = arr[i];
     }
   }
@@ -20,15 +20,15 @@ public class UnitTest {
   
   public int totalCorrect(){
     int totalCorrect = 0;
-    for(int i = 0; i <= studentAns.length; i++){
-      if(answerKey[i] == studentAns[i]) totalCorrect++;
+    for(int i = 0; i < studentAns.length; i++){
+      if(answerKey[i].equals(studentAns[i])) totalCorrect++;
     }
     return totalCorrect;
   }
 
   public int totalMistakes(){
     int totalMistakes = 0;
-    for(int i = 0; i <= studentAns.length; i++){
+    for(int i = 0; i < studentAns.length; i++){
       if(answerKey[i] != studentAns[i]) totalMistakes++;
     }
     return totalMistakes;
@@ -36,8 +36,8 @@ public class UnitTest {
 
   public boolean isPassing(){
     int totalCorrect = 0;
-    for(int i = 0; i <= studentAns.length; i++){
-      if(answerKey[i] == studentAns[i]) totalCorrect++;
+    for(int i = 0; i < studentAns.length; i++){
+      if(answerKey[i].equals(studentAns[i])) totalCorrect++;
     }
     return totalCorrect >= 14;
   }
@@ -45,9 +45,9 @@ public class UnitTest {
   public String toString(){
     String column = "";
 
-    for(int i = 0; i <= answerKey.length; i++){
-      column += "1." + answerKey[i] + "\t1." + studentAns[i];
-      System.out.println("");
+    for(int i = 0; i < answerKey.length; i++){
+      column +=(i+1) + "." + answerKey[i] + "\t" + (i+1) + "." + studentAns[i] + "\n";
+      
     }
     return column;
   }
